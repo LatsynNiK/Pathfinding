fb18bd6e51e2bc15a34c274fa58ee2714a4b200b
LatsynNiK <lastynn@mail.ru>
2022-05-14 20:21:11 +0300

    output to file

--- PathfindingAnalyzer/Program.cs
+++ PathfindingAnalyzer/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using Common;
 using MazeGenerator;
 using Pathfinding;
 
@@ -25,6 +24,8 @@ namespace PathfindingAnalyzer
             var numberOfMazes = 5;
             
             var analysisPrinter = new AnalysisPrinter();
+            
+            
             var analysisParameter1 = new AnalysisParameter()
             {
                 Name = "First",
@@ -34,30 +35,10 @@ namespace PathfindingAnalyzer
                 IsPerfectMaze = true,
                 // PercentOfWalls = 0
             };
-            //Console.WriteLine("Start analysis 1");
             var result1 = analyzer.RunAnalysis(analysisParameter1);
-            
             Console.Write(analysisPrinter.Print(analysisParameter1, result1));
-            //Console.WriteLine("Finish analysis 1");
-            //Console.WriteLine();
-            //Console.WriteLine("Result table");
-            //foreach (var pathFinder in result1.PathfindingStatistics.Keys)
-            //{
-            //    Console.WriteLine($"Pathfinder: {pathFinder.GetType()}");
-            //    Console.WriteLine($"\tMaze size\t\tTime (Milliseconds)");
-            //    foreach (var line in result1.PathfindingStatistics[pathFinder])
-            //    {
-            //        Console.WriteLine($"\t{line.Key}\t\t\t{line.Value}");
-            //    }
-            //}
-
             
-            //textWriter.WriteLine("1111222333;");
-
-            //textWriter.Close();
-            //Console.Write(textWriter);
-
-
+            
             var analysisParameter2 = new AnalysisParameter()
             {
                 Name = "Second",
@@ -68,40 +49,16 @@ namespace PathfindingAnalyzer
                 PercentOfWalls = 5,
                 StartPosition = CheckpointPosition.Center
             };
-            //Console.WriteLine("Start analysis 2");
             var result2 = analyzer.RunAnalysis(analysisParameter2);
-            //Console.WriteLine("Finish analysis 2");
-            //Console.WriteLine();
-            //Console.WriteLine("Result table");
-            //foreach (var pathFinder in result2.PathfindingStatistics.Keys)
-            //{
-            //    Console.WriteLine($"Pathfinder: {pathFinder.GetType()}");
-            //    Console.WriteLine($"\tMaze size\t\tTime (Milliseconds)");
-            //    foreach (var line in result2.PathfindingStatistics[pathFinder])
-            //    {
-            //        Console.WriteLine($"\t{line.Key}\t\t\t{line.Value}");
-            //    }
-            //}
-
-            
             Console.Write(analysisPrinter.Print(analysisParameter2, result2));
 
 
-            //var mazeGenerator = new EllerMazeGenerator();
-            //var maze = mazeGenerator.Generate(new MazeGeneratorOptions()
-            //{
-            //    Height = 50,
-            //    Width = 50,
-            //    PercentOfWalls = 25,
-            //    IsPerfectMaze = false
-            //});
-            //var mazePrinter = new MazePrinter();
-            //mazePrinter.AddMazeLayer(maze);//.AddStartAndFinish(maze.Start, maze.Finish).Print();
 
-            //var aStarPathFinder = new AStarPathFinder();
-            //var aStarPath = aStarPathFinder.FindPath(maze);
-            //mazePrinter.ClearPaths().AddPathLayer(aStarPath, "+ ").AddStartAndFinish(maze.Start, maze.Finish).Print();
-            //Console.WriteLine(); Console.WriteLine();
+            using (var sw = new StreamWriter("../../../../results.txt"))
+            {
+                sw.WriteLine(analysisPrinter.Print(analysisParameter1, result1));
+                sw.WriteLine(analysisPrinter.Print(analysisParameter2, result2));
+            }
 
             Console.ReadKey();
         }
